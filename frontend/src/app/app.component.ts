1eff1d3831d6671a9b0699f4a25afeea0d2aceb7
Dennis Mei <dmei22@hotmail.com>
2025-12-02 10:04:57 +0100

    Adjusted edit buttons

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "./component/navbar/navbar.component";
 import {RouterOutlet} from "@angular/router";
-import {BreadcrumbComponent} from "./component/breadcrumb/breadcrumb.component";
 
 @Component({
   selector: 'app-root',
